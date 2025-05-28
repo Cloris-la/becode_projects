11e9a264d07f21cf77f6c1e16b72a7bd5f7a7d03
Cloris-la <cfang763@gmail.com>
2025-05-28 16:37:25 +0200

    First time to submit code on these tools,hope it's ok if it's not ritght.

---
+++ 27_05_2025.py
@@ -0,0 +1,156 @@
+Microsoft Windows [Version 10.0.26100.4061]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\cloris\Documents\GitHub\BXL-Bouman-9>cd ..   
+
+C:\Users\cloris\Documents\GitHub>cd ..
+
+C:\Users\cloris\Documents>cd ..
+
+C:\Users\cloris>cd Desktop
+
+C:\Users\cloris\Desktop>dir
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+
+ Directory of C:\Users\cloris\Desktop
+
+05/28/2025  03:09 PM    <DIR>          .
+05/28/2025  12:36 PM    <DIR>          ..
+09/24/2024  10:58 AM             4,232 Book1.ods
+03/07/2025  11:28 PM           705,868 ChenFang CV picture.jpg
+03/07/2025  11:30 PM         6,212,033 ChenFang's CV.pdf
+08/26/2024  02:34 PM         1,837,493 ChenFang's Materials(1).pdf
+08/26/2024  09:12 AM             6,230 Cloris CV picture.jpg
+05/27/2025  11:00 AM             2,250 Discord.lnk
+05/27/2025  02:43 PM             2,376 GitHub Desktop.lnk
+04/03/2025  04:34 AM             2,350 Microsoft Edge.lnk
+05/27/2025  02:32 PM             1,405 Visual Studio Code.lnk
+11/19/2024  09:39 PM    <DIR>          档案材料
+12/09/2024  10:03 PM             1,055 百度网盘.lnk
+              10 File(s)      8,775,292 bytes
+               3 Dir(s)  266,430,431,232 bytes free
+
+C:\Users\cloris\Desktop>type nul >text.txt
+
+C:\Users\cloris\Desktop>vim text.txt
+
+C:\Users\cloris\Desktop>mkdir Octocat
+
+C:\Users\cloris\Desktop>cd Octocat
+
+C:\Users\cloris\Desktop\Octocat>type nul >yolo.md
+
+C:\Users\cloris\Desktop\Octocat>cd yolo.md
+The directory name is invalid.
+
+C:\Users\cloris\Desktop\Octocat>dir /s *yolo.md"
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+
+ Directory of C:\Users\cloris\Desktop\Octocat
+
+05/28/2025  03:15 PM                 0 yolo.md
+               1 File(s)              0 bytes
+
+     Total Files Listed:
+               1 File(s)              0 bytes
+               0 Dir(s)  266,429,341,696 bytes free
+
+C:\Users\cloris\Desktop\Octocat>dir
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+
+ Directory of C:\Users\cloris\Desktop\Octocat
+
+05/28/2025  03:15 PM    <DIR>          .
+05/28/2025  03:14 PM    <DIR>          ..
+05/28/2025  03:15 PM                 0 yolo.md
+               1 File(s)              0 bytes
+               2 Dir(s)  266,428,350,464 bytes free
+
+C:\Users\cloris\Desktop\Octocat>vim yolo.md
+
+C:\Users\cloris\Desktop\Octocat>type nul >README.md
+
+C:\Users\cloris\Desktop\Octocat>vim README.md
+
+C:\Users\cloris\Desktop\Octocat>type nul >FangChen.md
+
+C:\Users\cloris\Desktop\Octocat>vim FangChen.md
+
+C:\Users\cloris\Desktop\Octocat>del text.txt
+Could Not Find C:\Users\cloris\Desktop\Octocat\text.txt
+
+C:\Users\cloris\Desktop\Octocat>cd Desktop
+The system cannot find the path specified.
+
+C:\Users\cloris\Desktop\Octocat>cd
+C:\Users\cloris\Desktop\Octocat
+
+C:\Users\cloris\Desktop\Octocat>cd ..
+
+C:\Users\cloris\Desktop>del text.txt
+
+C:\Users\cloris\Desktop>dir Octocat
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+
+ Directory of C:\Users\cloris\Desktop\Octocat
+
+05/28/2025  03:36 PM    <DIR>          .
+05/28/2025  03:37 PM    <DIR>          ..
+05/28/2025  03:36 PM               282 FangChen.md
+05/28/2025  03:28 PM               208 README.md
+05/28/2025  03:22 PM                10 yolo.md
+               3 File(s)            500 bytes
+               2 Dir(s)  266,414,657,536 bytes free
+
+C:\Users\cloris\Desktop>mkdir Skills
+
+C:\Users\cloris\Desktop>dir Skills
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+
+ Directory of C:\Users\cloris\Desktop\Skills
+
+05/28/2025  03:38 PM    <DIR>          .
+05/28/2025  03:38 PM    <DIR>          ..
+               0 File(s)              0 bytes
+               2 Dir(s)  266,422,603,776 bytes free
+
+C:\Users\cloris\Desktop>cd Skills
+
+C:\Users\cloris\Desktop\Skills>type nul >successful.md
+
+C:\Users\cloris\Desktop\Skills>vim successful.md
+
+C:\Users\cloris\Desktop\Skills>cd ..
+
+C:\Users\cloris\Desktop>mkdir becode_projects
+
+C:\Users\cloris\Desktop>cd ..       
+
+C:\Users\cloris>Desktop
+'Desktop' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\cloris>de Desktop
+'de' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\cloris>cd Desktop
+
+C:\Users\cloris\Desktop>My_Project 
+'My_Project' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\cloris\Desktop>cd My_Project
+The system cannot find the path specified.
+
+C:\Users\cloris\Desktop>cd becode_project
+The system cannot find the path specified.
+
+C:\Users\cloris\Desktop>cd becode_projects
+
+C:\Users\cloris\Desktop\becode_projects>
