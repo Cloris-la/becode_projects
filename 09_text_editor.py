400b39ad176d2add16562494e982adf25ef7d114
Cloris-la <cfang763@gmail.com>
2025-05-29 11:30:50 +0200

    there is a problem of read vimtutor,I didn't find a way

---
+++ 09_text_editor.py
@@ -0,0 +1,98 @@
+Microsoft Windows [Version 10.0.26100.4061]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\cloris\Documents\GitHub\BXL-Bouman-9>D:
+
+D:\>cd projects
+
+D:\Projects>cd becode_projects
+
+D:\Projects\becode_projects>echo "I need to be created.Just because!" >text.txt
+
+D:\Projects\becode_projects>vim text.txt
+
+D:\Projects\becode_projects>notepad text.txt
+
+D:\Projects\becode_projects>vimtutor
+'vimtutor' is not recognized as an internal or external command,
+operable program or batch file.
+
+D:\Projects\becode_projects>C:
+
+C:\Users\cloris\Documents\GitHub\BXL-Bouman-9>c:
+
+C:\Users\cloris\Documents\GitHub\BXL-Bouman-9>cd ..
+
+C:\Users\cloris\Documents\GitHub>cd ..
+
+C:\Users\cloris\Documents>cd ..
+
+C:\Users\cloris>cd ..
+
+C:\Users>cd ..
+
+C:\>Program Files\Git\usr\bin\vimtutor   
+'Program' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\>cd Program Files  
+
+C:\Program Files>cd Git
+
+C:\Program Files\Git>dir /s vimtuor
+ Volume in drive C is OS
+ Volume Serial Number is A4C0-FB46
+File Not Found
+
+C:\Program Files\Git>cd usr
+
+C:\Program Files\Git\usr>cd bin
+
+C:\Program Files\Git\usr\bin>vimtutor
+'vimtutor' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Program Files\Git\usr\bin>cd vimtutor
+The directory name is invalid.
+
+C:\Program Files\Git\usr\bin>C:
+
+C:\Program Files\Git\usr\bin>c:
+
+C:\Program Files\Git\usr\bin>cd
+C:\Program Files\Git\usr\bin
+
+C:\Program Files\Git\usr\bin>c:
+
+C:\Program Files\Git\usr\bin>C:
+
+C:\Program Files\Git\usr\bin>cd \  
+
+C:\>cd "C:\Program Files\Git\usr\bin"
+
+C:\Program Files\Git\usr\bin>vimtutor
+'vimtutor' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Program Files\Git\usr\bin>vimtutor
+'vimtutor' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Program Files\Git\usr\bin>.\vimtutor
+'.\vimtutor' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Program Files\Git\usr\bin>dir vimtutor
+ Volume in drive C is OS
+
+ Directory of C:\Program Files\Git\usr\bin
+
+ Directory of C:\Program Files\Git\usr\bin
+
+03/17/2025  07:08 AM             4,858 vimtutor
+               1 File(s)          4,858 bytes
+               0 Dir(s)  272,077,942,784 bytes free
+
+C:\Program Files\Git\usr\bin>D:
+
+D:\Projects\becode_projects>
